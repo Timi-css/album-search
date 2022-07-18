77887a91d47451864a365aef97e0fd86030efa4a
Timi <alabitimi65@gmail.com>
2022-07-17 20:42:10 -0600

    fetched data using axios

--- react-albums/src/components/LiveSearch.js
+++ react-albums/src/components/LiveSearch.js
@@ -1,4 +1,5 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
+import Axios from "axios";
 
 import SearchBar from "components/SearchBar";
 import Results from "components/Results";
@@ -7,13 +8,21 @@ export default function LiveSearch(props) {
   const [term, setTerm] = useState("");
   const [results, setResults] = useState([]);
 
+  useEffect(() => {
+    const testURL = `https://search-itunes.vercel.app/?term=${term}&country=CA&media=music&entity=album&attribute=artistTerm`;
+    Axios.get(testURL).then((response) => {
+      setResults([...response.data.results]);
+      console.log(response.data.results);
+    });
+  }, [term]);
+
   return (
     <Fragment>
       <header className="logo">
         <img src="images/brand.png" alt="Brand" />
       </header>
       <main>
-        <SearchBar onSearch={term => setTerm(term)} />
+        <SearchBar onSearch={(term) => setTerm(term)} />
         <Results results={results} />
       </main>
     </Fragment>
